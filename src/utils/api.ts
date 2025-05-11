25c49ef87da2263ca69dea23aef8894fced1af71
IlushinGL <ilushingl@yandex.ru>
2025-05-11 17:58:02 +0300

    refactor: typeScriptr везде, кроме services

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -9,28 +9,37 @@ const AUTH_REGISTER_EP = '/auth/register';
 const AUTH_PSWDFORGOT_EP = '/password-reset';
 const AUTH_PSWDRESET_EP = '/password-reset/reset';
 
-const getResponce = (res) => {
-	if (typeof res.ok === 'undefined') {
-		return res;
-	} else if (res.ok) {
-		return res.json();
-	}
-	return res.json().then((err) => Promise.reject(err));
+import {
+	TAnsAuth,
+	TAnsInfo,
+	TAnsToken,
+	TAnsUser,
+	TUserConfirm,
+	TUserEmail,
+	TUserLogIn,
+	TUserReg,
+	TIngredient,
+} from './types';
+
+const getResponce = <T>(res: Response): Promise<T> => {
+	return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 	// return Promise.reject(`Ошибка ${res.status}`);
 };
 
-const getIngredients = () => {
-	return fetch(BASE_URL + INGREDIENTS_EP).then((res) => getResponce(res));
+const getIngredients = (): Promise<TIngredient[]> => {
+	return fetch(BASE_URL + INGREDIENTS_EP).then((res) =>
+		getResponce<TIngredient[]>(res)
+	);
 };
 
-const logOut = () => {
+const logOut = (): Promise<TAnsInfo> => {
 	localStorage.removeItem('isForgot');
 	return fetch(BASE_URL + AUTH_LOGOUT_EP, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json;charset=utf-8' },
 		body: JSON.stringify({ token: localStorage.getItem('refreshToken') }),
 	})
-		.then((res) => getResponce(res))
+		.then((res) => getResponce<TAnsInfo>(res))
 		.then((answer) => {
 			if (!answer.success) {
 				return Promise.reject(answer);
@@ -41,7 +50,7 @@ const logOut = () => {
 		});
 };
 
-const logIn = (data) => {
+const logIn = (data: TUserLogIn): Promise<TAnsAuth> => {
 	localStorage.removeItem('isForgot');
 	return fetch(BASE_URL + AUTH_LOGIN_EP, {
 		method: 'POST',
@@ -51,7 +60,7 @@ const logIn = (data) => {
 			password: data.password,
 		}),
 	})
-		.then((res) => getResponce(res))
+		.then((res) => getResponce<TAnsAuth>(res))
 		.then((answer) => {
 			if (!answer.success) {
 				return Promise.reject(answer);
@@ -62,7 +71,7 @@ const logIn = (data) => {
 		});
 };
 
-const pswdForgot = (data) => {
+const pswdForgot = (data: TUserEmail): Promise<TAnsInfo> => {
 	localStorage.removeItem('isForgot');
 	return fetch(BASE_URL + AUTH_PSWDFORGOT_EP, {
 		method: 'POST',
@@ -71,17 +80,17 @@ const pswdForgot = (data) => {
 			email: data.email,
 		}),
 	})
-		.then((res) => getResponce(res))
+		.then((res) => getResponce<TAnsInfo>(res))
 		.then((answer) => {
 			if (!answer.success) {
-				return false;
+				return Promise.reject(answer);
 			}
-			localStorage.setItem('isForgot', true);
-			return true;
+			localStorage.setItem('isForgot', 'true');
+			return answer;
 		});
 };
 
-const pswdReset = (data) => {
+const pswdReset = (data: TUserConfirm): Promise<TAnsInfo> => {
 	return fetch(BASE_URL + AUTH_PSWDRESET_EP, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json;charset=utf-8' },
@@ -90,17 +99,38 @@ const pswdReset = (data) => {
 			token: data.code,
 		}),
 	})
-		.then((res) => getResponce(res))
+		.then((res) => getResponce<TAnsInfo>(res))
 		.then((answer) => {
 			if (!answer.success) {
-				return false;
+				return Promise.reject(answer);
 			}
 			localStorage.removeItem('isForgot');
-			return true;
+			return answer;
 		});
 };
 
-const addOrder = (ingredientsArr) => {
+const setUser = (data: TUserReg): Promise<TAnsAuth> => {
+	return fetch(BASE_URL + AUTH_REGISTER_EP, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json;charset=utf-8' },
+		body: JSON.stringify({
+			email: data.email,
+			password: data.password,
+			name: data.name,
+		}),
+	})
+		.then((res) => getResponce<TAnsAuth>(res))
+		.then((answer) => {
+			if (!answer.success) {
+				return Promise.reject(answer);
+			}
+			localStorage.setItem('refreshToken', answer.refreshToken);
+			localStorage.setItem('accessToken', answer.accessToken);
+			return answer;
+		});
+};
+
+const addOrder = (ingredientsArr: []): Promise<TAnsInfo> => {
 	return fetchWithRefresh(BASE_URL + ORDERS_EP, {
 		method: 'POST',
 		headers: {
@@ -108,20 +138,10 @@ const addOrder = (ingredientsArr) => {
 			authorization: localStorage.getItem('accessToken'),
 		},
 		body: JSON.stringify({ ingredients: ingredientsArr }),
-	}).then((res) => getResponce(res));
+	}).then((res) => getResponce<TAnsInfo>(res));
 };
 
-const getUser = () => {
-	return fetchWithRefresh(BASE_URL + AUTH_USER_EP, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json;charset=utf-8',
-			authorization: localStorage.getItem('accessToken'),
-		},
-	}).then((res) => getResponce(res));
-};
-
-const updateUser = (data) => {
+const updateUser = (data: TUserReg): Promise<TAnsUser> => {
 	return fetchWithRefresh(BASE_URL + AUTH_USER_EP, {
 		method: 'PATCH',
 		headers: {
@@ -133,57 +153,50 @@ const updateUser = (data) => {
 			password: data.password,
 			name: data.name,
 		}),
-	}).then((res) => getResponce(res));
+	}).then((res) => getResponce<TAnsUser>(res));
 };
 
-const setUser = (data) => {
-	return fetch(BASE_URL + AUTH_REGISTER_EP, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json;charset=utf-8' },
-		body: JSON.stringify({
-			email: data.email,
-			password: data.password,
-			name: data.name,
-		}),
-	})
-		.then((res) => getResponce(res))
-		.then((answer) => {
-			if (!answer.success) {
-				return Promise.reject(answer);
-			}
-			localStorage.setItem('refreshToken', answer.refreshToken);
-			localStorage.setItem('accessToken', answer.accessToken);
-			return answer;
-		});
+const getUser = (): Promise<TAnsUser> => {
+	return fetchWithRefresh(BASE_URL + AUTH_USER_EP, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+			authorization: localStorage.getItem('accessToken'),
+		},
+	}).then((res) => getResponce<TAnsUser>(res));
 };
 
-const refreshToken = () => {
+const refreshToken = (): Promise<TAnsToken> => {
 	return fetch(BASE_URL + AUTH_TOKEN_EP, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json;charset=utf-8' },
 		body: JSON.stringify({ token: localStorage.getItem('refreshToken') }),
 	})
-		.then((res) => getResponce(res))
-		.then((refreshData) => {
-			if (!refreshData.success) {
-				return Promise.reject(refreshData);
+		.then((res) => getResponce<TAnsToken>(res))
+		.then((freshData) => {
+			if (!freshData.success) {
+				return Promise.reject(freshData);
 			}
-			localStorage.setItem('refreshToken', refreshData.refreshToken);
-			localStorage.setItem('accessToken', refreshData.accessToken);
-			return refreshData;
+			localStorage.setItem('refreshToken', freshData.refreshToken);
+			localStorage.setItem('accessToken', freshData.accessToken);
+			return freshData;
 		});
 };
 
-const fetchWithRefresh = async (url, options) => {
+const fetchWithRefresh = async (
+	url: string,
+	options: any
+): Promise<Response> => {
 	try {
 		const res = await fetch(url, options);
 		return await getResponce(res);
 	} catch (err) {
-		if (err.message === 'jwt expired') {
-			const refreshData = await refreshToken();
-			options.headers.authorization = refreshData.accessToken;
+		const isErr = err instanceof Error;
+		if (isErr && err.message === 'jwt expired') {
+			const freshData = await refreshToken();
+			options.headers.authorization = freshData.accessToken;
 			const res = await fetch(url, options);
-			return await getResponce(res);
+			return await getResponce<Response>(res);
 		} else {
 			return Promise.reject(err);
 		}
